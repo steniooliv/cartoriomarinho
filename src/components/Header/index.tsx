b32433241b1d5b2c8935b39fbdbb4c629b416729
Stenio Oliveira <steniooliv@gmail.com>
2021-10-06 15:42:58 -0300

    fix header logo

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -4,11 +4,11 @@ export default function Header() {
   return (
     <header className={styles.headerContainer}>
       <div className={styles.headerContent}>
-        <img src="/assets/logo.svg" alt="Cartório Marinho" />
+        <img src="/assets/logo.png" alt="Cartório Marinho" />
         <nav>
-          <a>Serviços</a>
-          <a>Quem Somos</a>
-          <a>Contato</a>
+          <a href="#services">Serviços</a>
+          <a href="#about">Quem Somos</a>
+          <a href="#contact">Contato</a>
         </nav>
       </div>
     </header>
