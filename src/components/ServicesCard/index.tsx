aa445a5ba3a2517f8e5251ecd0b212495eaad8d5
Stenio Oliveira <steniooliv@gmail.com>
2021-10-06 15:43:30 -0300

    change services assets svg to png

--- src/components/ServicesCard/index.tsx
+++ src/components/ServicesCard/index.tsx
@@ -6,12 +6,24 @@ interface Props {
   description: string
 }
 
-export default function ServicesCard(props : Props) {
-   return (
+export default function ServicesCard(props: Props) {
+
+
+  return (
     <div className={styles.cardContent}>
       <img src={props.icon} alt="" />
       <h1>{props.title}</h1>
-      <p>{props.description}</p>
+      <p>
+        {
+          props.description.length > 300
+          ?
+            props.description.substring(0, 300) + '... '
+        
+          :
+            props.description
+        }
+        <a>Saiba mais</a>
+      </p>
     </div>
   )
 }
