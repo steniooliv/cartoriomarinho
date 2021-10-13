45ba493e49ef74118476800bbe6055bfafa3d5e5
Stenio Oliveira <steniooliv@gmail.com>
2021-10-13 11:44:36 -0300

    add modal on service card

--- src/components/ServicesCard/index.tsx
+++ src/components/ServicesCard/index.tsx
@@ -1,6 +1,9 @@
+import { useState } from 'react';
+import Modal from '../Modal';
 import styles from './styles.module.scss';
 
 interface Props {
+  index: number
   icon: string,
   title: string,
   description: string
@@ -8,23 +11,37 @@ interface Props {
 
 export default function ServicesCard(props: Props) {
 
+  const [isOpen, setIsOpen] = useState(false);
+
+  function handleModalOpen() {
+    setIsOpen(true);
+  }
+
+  function handleModalClose() {
+    setIsOpen(false);
+  }
+
 
   return (
-    <div className={styles.cardContent}>
-      <img src={props.icon} alt="" />
-      <h1>{props.title}</h1>
-      <p>
-        {
-          props.description.length > 300
-          ?
-            props.description.substring(0, 300) + '... '
-        
-          :
-            props.description
-        }
-        <a>Saiba mais</a>
-      </p>
-    </div>
+    <>
+      <div className={styles.cardContent}>
+        <img src={props.icon} alt="" />
+        <h1>{props.title}</h1>
+        <p>
+          {
+            props.description.length > 300
+            ?
+              props.description.substring(0, 300) + '... '
+          
+            :
+              props.description
+          }
+          <a onClick={handleModalOpen}>Saiba mais</a>
+        </p>
+      </div>
+
+      <Modal key={props.index} id={props.index} isOpen={isOpen} handleClose={handleModalClose} />
+    </>
   )
 }
 
